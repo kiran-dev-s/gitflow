cc8af09f3b0bb391aaca1dd188e0d869d0eb64fa
Suraj Vishwakarma <suraj.vishwakarma.ext@nmims.edu>
2024-09-12 13:05:46 +0530

    Port console

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({
 	extended: false
 }));
-
+console.log('Port  =>', port);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
